e5571aa166e0a5b8ba8ae2f69cfeec913b835b4e
root15hyt <root15hyt@gmail.com>
2023-12-24 13:54:01 +0900

    App.js contained html-util.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 console.log("App.js: loaded");
+// elementタグ関数をインポートする。
+import { element } from "./view/html-util.js";
+
 // Appという名前付きクラスをエクスポートする。
 export class App {
     constructor() {
@@ -6,16 +9,27 @@ export class App {
     }
 
     mount() {
-        // id="js-form"の要素を取得
+        // id="js-form", "js-form-input", "js-todo-list", "js-todo-count"の要素を取得
         const formElement = document.querySelector("#js-form");
-        // id="js-form-input"の要素を取得
         const inputElement = document.querySelector("#js-form-input");
+        const containerElement = document.querySelector("#js-todo-list");
+        const todoItemCountElement = document.querySelector("#js-todo-count");
+        // Todoアイテム数
+        let todoItemCount = 0;
         // form要素から発生したsubmitイベントを受け取る
         formElement.addEventListener("submit", (event) => {
             // イベントが発生したときに呼ばれるコールバック関数
             // submitイベントの本来の動作を止める。
             event.preventDefault();
-            console.log(`入力欄の値: ${inputElement.value}`);
+            // 追加するTodoアイテムの要素（li要素）を作成する。
+            const todoItemElement = element`<li>${inputElement.value}</li>`;
+            // Todoアイテムをcontainerに追加する。
+            containerElement.appendChild(todoItemElement);
+            // Todoアイテム数を+1して、表示されているテキストを更新する。
+            todoItemCount += 1;
+            todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+            // 入力欄を空文字列にしてリセットする。
+            inputElement.value = "";
         });
     }
 }
